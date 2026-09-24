cf61669c17256c4ce627d2d9a91d10ca213acc8b
taukkunen1 <68305908+taukkunen1@users.noreply.github.com>
2026-09-24 18:30:46 -0300

    Website security: harden session cookies and response headers

--- COServer/ConquerSite/Startup.cs
+++ COServer/ConquerSite/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace ConquerSite
 {
@@ -22,7 +23,15 @@ namespace ConquerSite
         {
             services.AddRazorPages();
             services.AddControllersWithViews();
-            services.AddSession();
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.Name = ".Placebo.Session";
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+                options.Cookie.SecurePolicy = Microsoft.AspNetCore.Http.CookieSecurePolicy.Always;
+                options.Cookie.SameSite = Microsoft.AspNetCore.Http.SameSiteMode.Lax;
+            });
             services.AddSingleton<DowloadsSettings>();
             services.AddSingleton<GlobalSettings>();
             services.AddHostedService<GlobalSettingsInitializer>();
@@ -42,11 +51,19 @@ namespace ConquerSite
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
+            app.Use(async (context, next) =>
+            {
+                context.Response.Headers["X-Content-Type-Options"] = "nosniff";
+                context.Response.Headers["X-Frame-Options"] = "DENY";
+                context.Response.Headers["Referrer-Policy"] = "strict-origin-when-cross-origin";
+                context.Response.Headers["Permissions-Policy"] = "camera=(), microphone=(), geolocation=()";
+                await next();
+            });
             app.UseStaticFiles();
             app.UseRouting();
+            app.UseSession();
             app.UseAuthentication();
             app.UseAuthorization();
-            app.UseSession();
 
             app.UseEndpoints(endpoints =>
             {
